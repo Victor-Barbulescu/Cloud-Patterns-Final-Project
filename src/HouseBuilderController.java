701f166a29ce555a8a6bec8b77c38064fdfacf8b
kleicha <kleicha@msoe.edu>
2024-12-06 12:59:05 -0600

    created controller

---
+++ src/HouseBuilderController.java
@@ -0,0 +1,57 @@
+/*
+ * Course: SWE2410-121
+ * Fall 2024-2025
+ * File header contains class HouseBuilderController
+ * Name: kleicha
+ * Created 12/6/2024
+ */
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+/**
+ * Course SWE2410-121
+ * Fall 2024-2025
+ * Class HouseBuilderController Purpose: TODO
+ *
+ * @author kleicha
+ * @version created on 12/6/2024 12:55 PM
+ */
+public class HouseBuilderController {
+    @FXML
+    private Button AddBackyardButton;
+
+    @FXML
+    private Button AddBasementButton;
+
+    @FXML
+    private Button AddBathroomButton;
+
+    @FXML
+    private Button AddGarageButton;
+
+    @FXML
+    private Button AddKitchenButton;
+
+    @FXML
+    private Button AddLivingroomButtom;
+
+    @FXML
+    private Button RemoveButton;
+
+    @FXML
+    private Button BuildButton;
+
+    @FXML
+    private Button RestartButton;
+
+    @FXML
+    private Label PriceOutputLabel;
+
+    @FXML
+    private Label RoomListLabel;
+
+    @FXML
+    private Label HouseDescriptionLabel;
+}
